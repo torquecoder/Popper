7428f5b59f3689b4a18e28a0aef0a81cc956e0c3
Shubham Singhal <torquecode@gmail.com>
2016-10-31 00:56:40 +0530

    Initial commit

---
+++ app/src/main/java/tech/torque/popper/Balloon.java
@@ -0,0 +1,72 @@
+package tech.torque.popper;
+
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+
+import tech.torque.popper.utils.PixelHelper;
+
+
+public class Balloon extends ImageView implements Animator.AnimatorListener, ValueAnimator.AnimatorUpdateListener {
+
+    private ValueAnimator mAnimator;
+
+    public Balloon(Context context) {
+        super(context);
+    }
+
+    public Balloon(Context context, int color, int rawHeight) {
+        super(context);
+
+        this.setImageResource(R.drawable.balloon);
+        this.setColorFilter(color);
+
+        int rawWidth = rawHeight / 2;
+
+        int dpHeight = PixelHelper.pixelsToDp(rawHeight, context);
+        int dpWidth = PixelHelper.pixelsToDp(rawWidth, context);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(dpWidth, dpHeight);
+        setLayoutParams(params);
+
+    }
+
+    public void releaseBalloon(int screenHeight, int duration) {
+        mAnimator = new ValueAnimator();
+        mAnimator.setDuration(duration);
+        mAnimator.setFloatValues(screenHeight, 0f);
+        mAnimator.setInterpolator(new LinearInterpolator());
+        mAnimator.setTarget(this);
+        mAnimator.addListener(this);
+        mAnimator.addUpdateListener(this);
+        mAnimator.start();
+    }
+
+
+    @Override
+    public void onAnimationStart(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+        setY((Float) valueAnimator.getAnimatedValue());
+    }
+}
---
+++ app/src/main/java/tech/torque/popper/MainActivity.java
@@ -0,0 +1,134 @@
+package tech.torque.popper;
+
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+
+import java.util.Date;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewGroup mContentView;
+    private int[] mBalloonColors = new int[3];
+    private int mNextColor = 0, mScreenWidth, mScreenHeight;
+
+    public static final int MIN_ANIMATION_DELAY = 500;
+    public static final int MAX_ANIMATION_DELAY = 1500;
+    public static final int MIN_ANIMATION_DURATION = 1000;
+    public static final int MAX_ANIMATION_DURATION = 8000;
+    private int mLevel;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBalloonColors[0] = Color.argb(255, 255, 0, 0);
+        mBalloonColors[1] = Color.argb(255, 0, 255, 0);
+        mBalloonColors[2] = Color.argb(255, 0, 0, 255);
+
+        getWindow().setBackgroundDrawableResource(R.drawable.modern_background);
+
+        mContentView = (ViewGroup) findViewById(R.id.activity_main);
+
+        ViewTreeObserver viewTreeObserver = mContentView.getViewTreeObserver();
+        if (viewTreeObserver.isAlive()) {
+            viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @Override
+                public void onGlobalLayout() {
+                    mContentView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    mScreenWidth = mContentView.getWidth();
+                    mScreenHeight = mContentView.getHeight();
+                }
+            });
+        }
+
+    }
+
+    private void startLevel() {
+        mLevel++;
+        BalloonLauncher launcher = new BalloonLauncher();
+        launcher.execute(mLevel);
+    }
+
+    public void goButtonClickHandler(View view) {
+        startLevel();
+    }
+
+    private class BalloonLauncher extends AsyncTask<Integer, Integer, Void> {
+
+        @Override
+        protected Void doInBackground(Integer... params) {
+
+            if (params.length != 1) {
+                throw new AssertionError(
+                        "Expected 1 param for current level");
+            }
+
+            int level = params[0];
+            int maxDelay = Math.max(MIN_ANIMATION_DELAY,
+                    (MAX_ANIMATION_DELAY - ((level - 1) * 500)));
+            int minDelay = maxDelay / 2;
+
+            int balloonsLaunched = 0;
+            while (balloonsLaunched < 3) {
+
+//              Get a random horizontal position for the next balloon
+                Random random = new Random(new Date().getTime());
+                int xPosition = random.nextInt(mScreenWidth - 200);
+                publishProgress(xPosition);
+                balloonsLaunched++;
+
+//              Wait a random number of milliseconds before looping
+                int delay = random.nextInt(minDelay) + minDelay;
+                try {
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            int xPosition = values[0];
+            launchBalloon(xPosition);
+        }
+
+    }
+
+    private void launchBalloon(int x) {
+
+        Balloon balloon = new Balloon(this, mBalloonColors[mNextColor], 150);
+
+        mNextColor = (mNextColor + 1) % 3;
+
+//      Set balloon vertical position and dimensions, add to container
+        balloon.setX(x);
+        balloon.setY(mScreenHeight + balloon.getHeight());
+        mContentView.addView(balloon);
+
+//      Let 'er fly
+        int duration = Math.max(MIN_ANIMATION_DURATION, MAX_ANIMATION_DURATION - (mLevel * 1000));
+        balloon.releaseBalloon(mScreenHeight, duration);
+
+    }
+
+
+
+
+}
+
+
+
+
